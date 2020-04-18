d815980e2554e88ad0c4872d5ec4fd4c3ae411a5
pop <pop@pops-mac.local>
2020-04-18 10:33:20 -0700

    Initial Commit

---
+++ Versi-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Versi-app
+//
+//  Created by pop on 4/18/20.
+//  Copyright © 2020 pop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
